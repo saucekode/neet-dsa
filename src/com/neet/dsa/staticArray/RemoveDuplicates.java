c700344c40c6f0c4717894cb0a6e3d6dddd33673
saucekode <saucekoder@gmail.com>
2024-10-27 23:30:43 +0100

    neet dsa - static array

---
+++ src/com/neet/dsa/staticArray/RemoveDuplicates.java
@@ -0,0 +1,8 @@
+package com.neet.dsa.staticArray;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ test/com/neet/dsa/staticArray/RemoveDuplicatesTest.java
@@ -0,0 +1,16 @@
+package com.neet.dsa.staticArray;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class RemoveDuplicatesTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
